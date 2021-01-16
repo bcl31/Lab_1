eda5e3e394d346c5ea0ac0dc6f8a8de90ae1c92b
bcl31 <17791229+bcl31@users.noreply.github.com>
2021-01-16 02:28:44 -0700

    First exercise done

---
+++ exercise1.py
@@ -0,0 +1,40 @@
+# this is a simple program that performs a number of processes on a provided list of integers, and informs the user of each process and the changes
+# caused by it
+
+def main():
+
+    # establish starting lists
+    numbers = [11, 25, 32, 4, 67, 18, 50, 11, 4, 11]
+    odd_numbers = []
+    print("The contents of object {0} are {1}".format(id(odd_numbers), odd_numbers))
+
+    # find the odd numbers, add them to a list, inform user of changes
+    find_odd(numbers, odd_numbers)
+    print("The contents of object {0} has been updated to {1}".format(id(odd_numbers), odd_numbers))
+
+    # sort the odd numbers, find and remove highest and lowest, inform user of highest and lowest values, as well as changes to odd_num list
+    odd_numbers.sort(reverse=True)
+    lowest_num = odd_numbers.pop()
+    highest_num = odd_numbers.pop(0)
+    print("The smallest odd number, {0} has been removed from the list of odd numbers".format(lowest_num))
+    print("The largest odd number, {0} has been removed from the list of odd numbers".format(highest_num))
+    print("The contents of object {0} has been updated to {1}".format(id(odd_numbers), odd_numbers))
+
+    # tell user size of list before and after removing the lowest odd number from the list
+    print("There are {0} numbers in the original list".format(len(numbers)))
+    full_remove(numbers, lowest_num)
+    print("After removing the smallest odd number, there are {0} numbers in the list: \n{1}".format(len(numbers), numbers))
+
+
+def find_odd(list_to_check, list_to_fill):
+    for number in list_to_check:
+        if (number % 2) != 0:
+            list_to_fill.append(number)
+
+
+def full_remove(list_to_check, item):
+    while item in list_to_check:
+        list_to_check.remove(item)
+
+
+main()
